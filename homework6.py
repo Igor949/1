0a20c2b6559e86eefd6b9faf89e8600baa262b6f
Igor949 <igor-syachin@mail.ru>
2024-06-05 22:03:51 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+my_dict={'Igor':1971,'Ira':1980,'Dima':2009}
+print(my_dict)
+print(my_dict["Ira"])
+my_dict["Sahsa"]=2016
+print(my_dict)
+my_dict['city']=1932
+my_dict['year']=53
+print(my_dict)
+del my_dict['Ira']
+print(my_dict)
+my_set={'комп',23,45,(2+3)}
+print(my_set)
+my_set=['комп',23,45,(2+3),'reset','del']
+my_set=set(my_set)
+print(my_set)
+print(my_set.remove(45))
+print(my_set)
